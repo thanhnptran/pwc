8d74727e90b438192922605c21dbfe023ba4c1be
thanhnptran <88019371+thanhnptran@users.noreply.github.com>
2024-08-29 21:22:38 +0700

    Update bds.py

--- bds.py
+++ bds.py
@@ -1,3 +1,5 @@
+##################### FIRST PART #####################
+
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service
@@ -21,3 +23,52 @@ with open(url_path, 'w') as file:
     file.write('\n'.join(url_list))
 
 driver.quit()
+
+################## END OF FIRST PART ##################
+
+##################### SECOND PART #####################
+
+bds_excel_path = r"C:\python\batdongsan\bds.xlsx"
+
+def write_dict_to_excel(dictionary, file_path=bds_excel_path):
+    suffix = '_’
+    ILLEGAL_CHARACTERS_RE = re.compile(r'[\000-\010]|[\013-\014]|[\016-\037]')
+    info_df = pd.DataFrame(dictionary).transpose().reset_index()
+    first_key = list(dictionary.keys())[0]
+    dicts = [element for element in list(info[first_key].keys()) if isinstance(info[first_key][element], dict)]
+    if len(dicts) > 0:
+        for idx, d in enumerate(dicts):
+            flat_df = pd.json_normalize(info_df[d])
+            info_df = info_df.join(flat_df,rsuffix=suffix*(idx+1))
+    info_df = info_df.map(lambda x: ILLEGAL_CHARACTERS_RE.sub(r'', x) if isinstance(x, str) else x)
+    info_df.to_excel(file_path,index=False)
+    print(fr'Done! Check output excel file at {file_path}')
+    return
+
+info = {} # this is a dictionary
+for idx, url in enumerate(url_list):
+    driver = webdriver.Chrome(service=service)
+    driver.get(url)
+    WebDriverWait(driver, 30).until(EC.visibility_of_element_located((By.XPATH,"//div[contains(@class,'re__project-toogle-box')]")))
+    project_name = driver.find_element(By.XPATH,"//h1[@class='re__project-name']").get_attribute('innerHTML')
+    project_address = driver.find_element(By.XPATH,"//div[@class='re__project-address']").text.replace(". Xem bản đồ",'')
+    items_html = driver.find_elements(By.XPATH,"//div[contains(@class,'js__project-box-info')]//div[@class='re__project-box-item']")
+    
+    details = {}
+    for item in items_html:
+        item_label = item.find_element(By.XPATH,"./label").get_attribute("innerHTML")
+        details[item_label] = item.find_element(By.XPATH,"./span").get_attribute("innerHTML")
+
+    info[idx] = {
+        'url': url,
+        'project_name': project_name,
+        'project_address': project_address,
+        'details': details
+    }
+    
+    driver.quit()
+    print(f"Done {idx}")
+
+write_dict_to_excel(info)
+
+################## END OF SECOND PART ##################
