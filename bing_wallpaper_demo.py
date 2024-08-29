690cfcd99153568c9e758475ffee9b753564a6ab
thanhnptran <88019371+thanhnptran@users.noreply.github.com>
2024-08-29 21:41:34 +0700

    Create bing_wallpaper_demo.py

---
+++ bing_wallpaper_demo.py
@@ -0,0 +1,22 @@
+import requests
+
+endpoint = 'https://bingw.jasonzeng.dev'
+headers_json = {
+    'content-type': 'application/json',
+    'accept': 'application/json'
+}
+params_json = {
+    'resolution': 'UHD',
+    'format': 'json',
+    'date': '20230115'
+}
+
+r = requests.get(endpoint, headers=headers_json, params=params_json)
+result = r.json()
+
+result['url']
+
+img_data = requests.get(result['url'], verify=False, stream=True)
+file_path = r"C:\python\20230115.jpg"
+with open(file_path, 'wb') as handler:
+    handler.write(img_data.content)
