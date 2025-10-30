cadcd76a5785e9118d3bb3dd54d3153545b9ff31
thanhnptran <88019371+thanhnptran@users.noreply.github.com>
2025-10-30 10:44:32 +0700

    Update bing_wallpaper.py

--- bing_wallpaper.py
+++ bing_wallpaper.py
@@ -19,14 +19,18 @@ def save_images(fromdate, todate, folder_path):
             'format': 'json',
             'date': f'{date}'
         }
-        r = requests.get(endpoint,headers=headers,params=params)
-        result = r.json()
+        try:
+            r = requests.get(endpoint,headers=headers,params=params)
+            result = r.json()
 
-        img_data = requests.get(result['url'], verify=False, stream=True)
-        with open(fr'{folder_path}\{date}.jpg', 'wb') as handler:
-            handler.write(img_data.content)
-        print(f'Done {date}')
+            img_data = requests.get(result['url'], verify=False, stream=True)
+            with open(fr'{folder_path}\{date}.jpg', 'wb') as handler:
+                handler.write(img_data.content)
+            print(f"Done {date}")
+        except Exception:
+            print(f"Error downloading {date}")
+        
         time.sleep(2)
 
 img_folder_path = r"C:\python\images" # remember to change to your local file path 
-save_images('20240801', '20240811', img_folder_path)
+save_images('20240801', '20240810', img_folder_path)
